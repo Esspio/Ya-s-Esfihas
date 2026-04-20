6793eea17f221b7ba7380aef0228148856fc86a7
Esspio <felipe.oliveira200085@gmail.com>
2026-04-19 21:30:49 -0300

    Criacao backend e conexao ao banco

---
+++ backend/src/main/java/com/esp/yas_esfihas/YasEsfihasApplication.java
@@ -0,0 +1,13 @@
+package com.esp.yas_esfihas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasEsfihasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasEsfihasApplication.class, args);
+	}
+
+}
